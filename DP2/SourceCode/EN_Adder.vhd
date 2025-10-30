e86a7d586d5d20f1e1689479809dfeb4d07e053b
Amytis Saghafi <amytis25@gmail.com>
2025-10-30 14:58:56 -0700

    added new architectures

--- DP2/SourceCode/EN_Adder.vhd
+++ DP2/SourceCode/EN_Adder.vhd
@@ -14,29 +14,8 @@ entity EN_Adder is
 	);
 end EN_Adder;
 
-architecture ripple of EN_Adder is
-	signal C : std_logic_vector (N-1 downto 0);
-	signal P,G : std_logic_vector (N-1 downto 0);
-	
-	begin
-	P(0) <= A(0) xor B(0);
-	G(0) <= A(0) and B(0);
-	S(0) <= P(0) xor Cin;
-	C(0) <= G(0) or (P(0) and Cin);
-	
-	rippleNetwork: for i in 1 to N-1 generate
-		P(i) <= A(i) xor B(i);
-		G(i)<= A(i) and B(i);
-		S(i) <= P(i) xor C(i-1);
-		C(i) <= G(i) or (P(i) and C(i-1));
-	end generate;
-	Cout <= C(N-1);
-	Ovfl <= (not (A(N-1) xor B(N-1))) and (A(N-1) xor S(N-1));
-	
-	
-end ripple;
-
-architecture FastRipple of EN_Adder is
+-- Quartus Optimized Ripple Adder
+architecture FastRipple of EN_Adder is 
 
 signal temp : unsigned(N downto 0);
 	begin
@@ -50,6 +29,24 @@ signal temp : unsigned(N downto 0);
 	 Ovfl <= (temp(N) xor temp(N-1));
 end FastRipple;
 
+-- RCA : Ripple Carry Adder
+architecture RCA of EN_Adder is 
+	signal C : std_logic_vector (N downto 0);
+	signal P,G : std_logic_vector (N-1 downto 0);
+	
+	begin
+	c(0) <= Cin;
+	g <= a and b;
+	p <= a xor b;
+	c(N downto 1) <= g or (p and c(N-1 downto 0));
+	S <= p xor c(n-1 downto 0);
+	Cout <= C(N);
+	Ovfl <= (not (A(N-1) xor B(N-1))) and (A(N-1) xor S(N-1));
+	
+	
+end ripple;
+
+-- CSA : Conditional Sum Adder
 architecture CSA of EN_Adder is 
 	constant N_half : integer := N / 2; 
 	signal Cout0, Cout1, Chalf : std_logic := '0';
@@ -59,36 +56,36 @@ architecture CSA of EN_Adder is
 	begin
 		recur : if N > 2 generate
 			begin 
-    CSAlow : entity work.EN_Adder(CSA)
-      generic map (N => N_half)
-      port map (
-        A => A(N_half-1 downto 0),
-        B => B(N_half-1 downto 0),
-        S => S(N_half-1 downto 0),
-        Cin => Cin,
-        Cout => Chalf,
-        Ovfl => open
-      );
-    CSAzero : entity work.EN_Adder(CSA)
-      generic map (N => N_half)
-      port map (
-        A    => A(N-1 downto N_half),
-        B    => B(N-1 downto N_half),
-        S    => sum0,
-        Cin  => '0',
-        Cout => Cout0,
-        Ovfl => open
-      );
-    CSAone : entity work.EN_Adder(CSA)
-      generic map (N => N_half)
-      port map (
-        A    => A(N-1 downto N_half),
-        B    => B(N-1 downto N_half),
-        S    => sum1,
-        Cin  => '1',
-        Cout => Cout1,
-        Ovfl => open
-      );
+			CSAlow : entity work.EN_Adder(CSA)
+				generic map (N => N_half)
+				port map (
+					A => A(N_half-1 downto 0),
+					B => B(N_half-1 downto 0),
+					S => S(N_half-1 downto 0),
+					Cin => Cin,
+					Cout => Chalf,
+					Ovfl => open
+				);
+			CSAzero : entity work.EN_Adder(CSA)
+				generic map (N => N_half)
+				port map (
+					A    => A(N-1 downto N_half),
+					B    => B(N-1 downto N_half),
+					S    => sum0,
+					Cin  => '0',
+					Cout => Cout0,
+					Ovfl => open
+				);
+			CSAone : entity work.EN_Adder(CSA)
+				generic map (N => N_half)
+				port map (
+					A    => A(N-1 downto N_half),
+					B    => B(N-1 downto N_half),
+					S    => sum1,
+					Cin  => '1',
+					Cout => Cout1,
+					Ovfl => open
+				);
 		end generate recur;
 		
 		leaf: if N = 2 generate
@@ -114,3 +111,19 @@ architecture CSA of EN_Adder is
 
 			
 end CSA;
+
+-- LATA : Look Ahead Tree Adder 
+architecture LATA of EN_Adder is 
+
+end LATA;
+
+-- BKA : Brent-Kung Adder
+architecture BKA of EN_Adder is 
+
+end BKA;
+
+-- LFA : Ladner Fischer Adder
+architecture LFA of EN_Adder is 
+
+end LFA;
+
