b1cefec1a94bfa2ca26b98284f204a8c679ca2c7
Amytis Saghafi <amytis25@gmail.com>
2025-11-12 18:34:40 -0800

    added functional .do files and updated testbench and got transcripts from functional tests

--- DP2/Simulation/Config_Adder.vhd
+++ DP2/Simulation/Config_Adder.vhd
@@ -9,26 +9,26 @@ use ieee.std_logic_1164.all;
 
 -- functional testing
 configuration CFG_FUNC_RCA of TB_Adder is
-  for behavioural
-    for DUT : EN_Adder use entity work.EN_Adder(RCA); end for;
+  for behavior
+    for DUT : TestUnit use entity work.EN_Adder(RCA); end for;
   end for;
 end configuration;
 
 configuration CFG_FUNC_CSA of TB_Adder is
-  for behavioural
-    for DUT : EN_Adder use entity work.EN_Adder(CSA); end for;
+  for behavior
+    for DUT : TestUnit use entity work.EN_Adder(CSA); end for;
   end for;
 end configuration;
 
 configuration CFG_FUNC_LACGT of TB_Adder is
-  for behavioural
-    for DUT : EN_Adder use entity work.EN_Adder(LACGT); end for;
+  for behavior
+    for DUT : TestUnit use entity work.EN_Adder(LACGT); end for;
   end for;
 end configuration;
 
 configuration CFG_FUNC_BKA of TB_Adder is
-  for behavioural
-    for DUT : EN_Adder use entity work.EN_Adder(BKA); end for;
+  for behavior
+    for DUT : TestUnit use entity work.EN_Adder(BKA); end for;
   end for;
 end configuration;
 
@@ -38,26 +38,26 @@ end configuration;
 -- Timing: bind to Quartus-generated .vho architecture, usually named 'structure'
 -- Make sure EN_Adder.vho is compiled *before* EN_Adder.vhd in ModelSim.
 configuration CFG_TIMING_RCA of TB_Adder is
-  for behavioural
-    for DUT : EN_Adder use entity work.EN_Adder(structure); end for;
+  for behavior
+    for DUT : TestUnit use entity work.EN_Adder(structure); end for;
   end for;
 end configuration;
 
 -- (Optional) Another timing candidate (e.g., BKA synthesized separately)
 configuration CFG_TIMING_CSA of TB_Adder is
-  for behavioural
-    for DUT : EN_Adder use entity work.EN_Adder(structure); end for;
+  for behavior
+    for DUT : TestUnit use entity work.EN_Adder(structure); end for;
   end for;
 end configuration;
 
 configuration CFG_TIMING_LACGT of TB_Adder is
-  for behavioural
-    for DUT : EN_Adder use entity work.EN_Adder(structure); end for;
+  for behavior
+    for DUT : TestUnit use entity work.EN_Adder(structure); end for;
   end for;
 end configuration;
 
 configuration CFG_TIMING_BKA of TB_Adder is
-  for behavioural
-    for DUT : EN_Adder use entity work.EN_Adder(structure); end for;
+  for behavior
+    for DUT : TestUnit use entity work.EN_Adder(structure); end for;
   end for;
 end configuration;
